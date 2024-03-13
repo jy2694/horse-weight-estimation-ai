2a8a59486f12c64f05d0f1a0d523b7c079974f76
gimjeyeon <wpdus2694@naver.com>
2024-03-13 16:34:58 +0900

    Initial commit

---
+++ server/src/main/java/kr/ac/wku/estimation/HorseWeightEstimationApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.wku.estimation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseWeightEstimationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorseWeightEstimationApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/kr/ac/wku/estimation/HorseWeightEstimationApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.wku.estimation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseWeightEstimationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
